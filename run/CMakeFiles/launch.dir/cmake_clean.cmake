7ba1d2f77166b5c424cbc8c54c477d134b80a974
Washim <washimneupane@outlook.com>
2020-07-06 23:51:45 +1000

    Initial files

---
+++ run/CMakeFiles/launch.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/launch.dir/home/wneupane/Documents/github/startup_idea/main.cpp.o"
+  "launch"
+  "launch.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/launch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
